8d0f060b191a9485dbbbcfae807d6b5b887b8a79
Laura <Laura@DESKTOP-BESSB6E>
2023-10-11 22:50:46 +0300

    Add project files.

---
+++ 2Lygis_Streams/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _2Lygis_Streams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string content = File.ReadAllText("path.txt");
+
+            File.WriteAllText("anotherPath.txt", content);
+
+            Console.WriteLine(content);
+
+            List<string> list = new List<string>() { "Labas rytas", "Labas vakaras" };
+
+            File.WriteAllLines("Pasisveikinimas.txt", list);
+
+            File.Copy("Pasisveikinimas.txt", "Hello.txt");
+
+
+
+            Uzduotis2();
+        }
+        public static void Uzduotis2()
+        {
+          List<string> pasakosEilutes =  File.ReadLines("Pasaka.txt").ToList(); 
+
+            foreach (string pasakosEil in pasakosEilutes)
+            {
+                Console.WriteLine(pasakosEil);
+            }
+        }
+    }
+}
